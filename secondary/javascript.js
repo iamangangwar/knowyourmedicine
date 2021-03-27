f0773c594c403ab8aea17a65ee0d7c442943384c
antaresiota <amangangwar084@gmail.com>
2021-03-28 03:11:27 +0530

    secondary pages

---
+++ secondary/javascript.js
@@ -0,0 +1,80 @@
+function myFunction1() {
+    var x = document.getElementById("cs1");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function myFunction2() {
+    var x = document.getElementById("cs2");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function myFunction3() {
+    var x = document.getElementById("cs3");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function myFunction4() {
+    var x = document.getElementById("cs4");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function myFunction5() {
+    var x = document.getElementById("cs5");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function myFunction6() {
+    var x = document.getElementById("cs6");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function myFunction7() {
+    var x = document.getElementById("cs7");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function myFunction8() {
+    var x = document.getElementById("cs8");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function myFunction9() {
+    var x = document.getElementById("cs9");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+function myFunction10() {
+    var x = document.getElementById("cs10");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
